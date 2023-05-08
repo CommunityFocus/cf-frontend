10d8b3688b600c482c531be5595043dc058f9615
Mario Nikhil Pereira <45009203+nmpereira@users.noreply.github.com>
2023-05-08 03:53:21 -0400

    update logic to add users in room

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -1,60 +1,59 @@
-import  { useState, useEffect } from 'react';
-import { socket } from './socket';
-import { ConnectionState } from './ConnectionState';
-import { ConnectionManager } from './ConnectionManager';
-import { Events } from './Events';
-import { TimerForm } from './TimerForm';
+import { useState, useEffect } from "react";
+import { socket } from "./socket";
+import { ConnectionState } from "./ConnectionState";
+import { ConnectionManager } from "./ConnectionManager";
+import { Events } from "./Events";
+import { TimerForm } from "./TimerForm";
 function Room() {
-    const [isConnected, setIsConnected] = useState(socket.connected);
-  const [timestamp, setTimestamp] = useState('0');
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [timestamp, setTimestamp] = useState("0");
   const [usersInRoom, setUsersInRoom] = useState(0);
 
   useEffect(() => {
+    const roomName = window.location.href.split("/")[4];
     function onConnect() {
-      socket.emit('join', window.location.href.split('/')[4]);
+      socket.emit("join", roomName);
+      console.log("join", roomName);
       setIsConnected(true);
     }
 
     function onDisconnect() {
+      socket.emit("disconnect", roomName);
       setIsConnected(false);
     }
 
-    function onTimerUpdate(value : string) {
-      console.log('timer', value);
+    function onTimerUpdate(value: string) {
+      console.log("timer", value);
       setTimestamp(value);
     }
     function onUsersInRoom(value: number) {
-      console.log('usersInRoom', value);
+      console.log("usersInRoom", value);
       setUsersInRoom(value);
     }
 
-    socket.on('connect', onConnect);
-    socket.on('disconnect', onDisconnect);
-    socket.on('timerUpdated', onTimerUpdate);
-    socket.on('usersInRoom', onUsersInRoom);
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("timerUpdated", onTimerUpdate);
+    socket.on("usersInRoom", onUsersInRoom);
 
     return () => {
-      socket.off('connect', onConnect);
-      socket.off('disconnect', onDisconnect);
-      socket.off('timerUpdated', onTimerUpdate);
-      socket.off('usersInRoom', onUsersInRoom);
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.off("timerUpdated", onTimerUpdate);
+      socket.off("usersInRoom", onUsersInRoom);
     };
   }, []);
 
-
   useEffect(() => {
-    console.log('timestamp', timestamp);
+    console.log("timestamp", timestamp);
     // update the document title, with roomName and timestamp
-    document.title = `${timestamp}-${window.location.href.split('/')[4]}`;
-  },[
-    timestamp
-  ]);
+    document.title = `${timestamp}-${window.location.href.split("/")[4]}`;
+  }, [timestamp]);
 
   useEffect(() => {
-    console.log('URL', window.location.href);
-    console.log('roomId:', window.location.href.split('/')[4]);
+    console.log("URL", window.location.href);
+    console.log("roomId:", window.location.href.split("/")[4]);
   }, [isConnected]);
-
 
   return (
     <>
--- src/components/socket.ts
+++ src/components/socket.ts
@@ -2,5 +2,12 @@ import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.NODE_ENV === 'production' ? window.location.origin : 'http://localhost:4000';
+const roomName = window.location.href.split("/")[4] || "default";
+console.log({roomName2:roomName});
 
-export const socket = io(URL);
+export const socket = io(URL,{
+    query: {
+        roomName: roomName
+    }
+
+});
