f3ca4e6886c4d11fb037d62db15674ca3f5e74c5
nmpereira <nikhilpereira25@gmail.com>
2023-05-12 00:56:31 -0400

    add env file to add backend url

--- src/components/socket.ts
+++ src/components/socket.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? window.location.origin : 'http://localhost:4000';
+const URL = import.meta.env.VITE_NODE_ENV==='production' ? import.meta.env.VITE_BACKEND_URL : 'http://localhost:4000';
 const roomName = window.location.href.split("/")[4] || "default";
 console.log({roomName2:roomName});
 
