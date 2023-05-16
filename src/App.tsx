1f8b6ecc7acc1ae17175dfd04a6de19cd7d151f1
mefdev <mohamedmeftah2018@gmail.com>
2023-05-16 03:17:08 +0100

    Reroute the user to '/:room' when they click

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-
-
-
+import { Link } from "react-router-dom";
 export default function App() {
-  
-
   return (
+    // reroute the user to "/:room" when they click the btn (mefdev)
     <div className="App">
-      
-    <h1>Hello</h1>
+      <Link to={"/:room"}><button>Start a timer</button></Link>
     </div>
   );
 }
