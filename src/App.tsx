7389bb0e347d745e936dfe56e556d3608cc6b9aa
mefdev <mohamedmeftah2018@gmail.com>
2023-05-16 03:17:08 +0100

    Reroute the user to '/:room' when they click

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import {SERVER_URL} from "../common/common"
+import { SERVER_URL } from "../common/common";
 export default function App() {
+  const [slugName, setSlugName] = useState<string>("");
 
-  const [slugName, setSlugName] = useState<string>("")
-  
-   useEffect(() => {
-      axios.get(`${SERVER_URL}/api/v1/getslug`)
-     .then((res) => {
-      setSlugName(res.data.string)
-     }).catch(e => {
-      console.error('getSlug:', e)
-     });
-  
- }, [])
+  useEffect(() => {
+    axios
+      .get(`${SERVER_URL}/api/v1/getslug`)
+      .then((res) => {
+        setSlugName(res.data.string);
+      })
+      .catch((e) => {
+        console.error("getSlug:", e);
+      });
+  }, []);
   return (
-    
     // reroute the user to a random slug when they click a btn
     <div className="App">
-      <Link to={`/${slugName}`}><button >Start a timer</button></Link>
+      <Link to={`/${slugName}`}>
+        <button>Start a timer</button>
+      </Link>
     </div>
   );
 }
