f7320d5be49833b8ae1d66b6e90d08838b7da492
nmpereira <nikhilpereira25@gmail.com>
2023-05-20 05:14:55 -0400

    add jest testing library

---
+++ jest.config.ts
@@ -0,0 +1,7 @@
+// source https://codingwithmanny.medium.com/quick-jest-setup-with-vitejs-react-typescript-82f325e4323f
+
+export default {
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  }
+}
--- src/components/socket.ts
+++ src/components/socket.ts
@@ -1,9 +1,10 @@
 import { io } from 'socket.io-client';
 
-// "undefined" means the URL will be computed from the `window.location` object
-const URL = import.meta.env.VITE_NODE_ENV==='production' ? import.meta.env.VITE_BACKEND_URL : 'http://localhost:4000';
+const NODE_ENV= process.env.VITE_NODE_ENV;
+const BACKEND_URL= process.env.BACKEND_URL||'null';
+
+const URL = NODE_ENV==='production' ? BACKEND_URL : 'http://localhost:4000';
 const roomName = window.location.href.split("/")[3] || "default";
-console.log({roomName2:roomName});
 
 export const socket = io(URL,{
     query: {
