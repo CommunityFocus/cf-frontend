b43e70cae4a30d81df6afedfc66ea68b01e82c2b
Mario Nikhil Pereira <45009203+nmpereira@users.noreply.github.com>
2023-05-21 01:26:26 -0400

    clean up timestamp file name, change server url (#5)
    * clean up timestamp file name
    
    * Update socket.ts
    
    * Update _redirects
    
    * Update _redirects

--- src/components/TimerForm.tsx
+++ src/components/TimerForm.tsx
@@ -5,27 +5,29 @@ export function TimerForm() {
   const [value, setValue] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  function onSubmit(event:React.FormEvent<HTMLFormElement>) {
+  function onSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     setIsLoading(true);
 
-    socket.timeout(1000).emit("startCountdown", {
-      roomName: window.location.href.split("/")[3],
-      durationInSeconds: parseInt(value),
-    }, () => {
-      setIsLoading(false);
-    });
+    socket.timeout(1000).emit(
+      "startCountdown",
+      {
+        roomName: window.location.href.split("/")[3],
+        durationInSeconds: parseInt(value),
+      },
+      () => {
+        setIsLoading(false);
+      }
+    );
   }
 
   return (
     <form onSubmit={onSubmit}>
-      <input onChange={(e) => setValue(e.target.value)} 
-        // has to be a number
+      <input
+        onChange={(e) => setValue(e.target.value)}
         value={value}
         type="number"
         placeholder="Enter something"
-
-
       />
 
       <button type="submit" disabled={isLoading}>
