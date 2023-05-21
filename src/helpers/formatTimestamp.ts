a4462c2fda353f0f6dd0e76554ffbac16591427e
intelagense <intelagense@gmail.com>
2023-05-21 09:32:46 -0500

    add display support for large hours, hide 00 hours

--- src/helpers/formatTimestamp.ts
+++ src/helpers/formatTimestamp.ts
@@ -1,11 +1,15 @@
 function formatTimestamp(timeRemaining: number) {
-	const hours = Math.floor((timeRemaining % (60 * 60 * 99)) / (60 * 60));
+	const hours = Math.floor(timeRemaining / (60 * 60));
 	const minutes = Math.floor((timeRemaining % (60 * 60)) / 60);
 	const seconds = Math.floor((timeRemaining % 60));
 	const padZeros = (num: number) => num.toString().padStart(2, '0');
-	const timestamp = `${padZeros(hours)}:${padZeros(minutes)}:${padZeros(seconds)}`;
-	return timestamp;
+
+	// Sheds "HH:" when duration is under 1 hour (3600 seconds)
+	if (timeRemaining > 3600) {
+		return `${padZeros(hours)}:${padZeros(minutes)}:${padZeros(seconds)}`;
+	} else {
+		return `${padZeros(minutes)}:${padZeros(seconds)}`
+	}
 }
 
 export default formatTimestamp;
-
