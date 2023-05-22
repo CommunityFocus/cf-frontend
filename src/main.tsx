fb431d76b88f4d2c1f5f9f7c6621f3babb7aec13
Mario Nikhil Pereira <45009203+nmpereira@users.noreply.github.com>
2023-05-22 01:52:21 -0400

    add daisyUI and tailwind css

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import Room from "./components/Room.tsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './main.css'
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
