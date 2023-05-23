1a5be1cd03e372cf99f02e0a86c806ab03721c95
mefdev <mohamedmeftah2018@gmail.com>
2023-05-23 16:29:29 +0100

    Reroute the user to a random url when they click a btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
 export default function App() {
+  
+  const [randomURL, setRandomURL] = useState("")
+   useEffect(() => {
+      axios.get("http://localhost:4000/randomid")
+     .then((res) => {
+      setRandomURL(res.data.string)
+     }).catch(e => {
+       console.log(e)
+     });
+  
+ }, [])
+ 
+   
   return (
-    // reroute the user to "/:room" when they click the btn (mefdev)
+    
+    // reroute the user to a random url when they click a btn
     <div className="App">
-      <Link to={"/:room"}><button>Start a timer</button></Link>
+      <Link to={`/${randomURL}`}><button >Start a timer</button></Link>
     </div>
   );
 }
