709cb0046ebe30191153f5398c2fe6575ec52942
Mario Nikhil Pereira <45009203+nmpereira@users.noreply.github.com>
2023-05-25 05:51:03 -0400

    Update link to button and use useNavigate

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { SERVER_URL } from "../common/common";
+
 export default function App() {
   const [slugName, setSlugName] = useState<string>("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios
-      .get(`${SERVER_URL}/api/v1/getslug`)
+      .get(`${SERVER_URL}/api/v1/getSlug`)
       .then((res) => {
-        setSlugName(res.data.string);
+        setSlugName(res.data.slug);
       })
-      .catch((e) => {
-        console.error("getSlug:", e);
+      .catch((err) => {
+        console.log(err);
       });
   }, []);
-  
-  return (
-    // reroute the user to a random slug when they click a btn
-    <div className="App">
-      <Link to={`/${slugName}`}>
-        <button>Start a timer</button>
-      </Link>
 
+  return (
+    <div className="App">
+      <h1>Community Focus</h1>
+      <button onClick={() => navigate(`/${slugName}`)}>Join a room</button>
     </div>
   );
 }
