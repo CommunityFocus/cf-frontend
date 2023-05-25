f0b0cf22f6b8f2b61f46abcc0440298f8498b995
Mario Nikhil Pereira <45009203+nmpereira@users.noreply.github.com>
2023-05-25 03:25:09 -0400

    add basic styling to home page

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,7 @@ export default function App() {
 
   return (
     <div className="App">
-      
-    <h1>Hello</h1>
+    <h1 className="text-3xl font-bold flex items-center justify-center h-screen">Community Focus</h1>
     </div>
   );
 }
