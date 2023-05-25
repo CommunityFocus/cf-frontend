d554389ee5ee1c2a17178f26708fd04b48a31bf1
MefDev <54311215+MefDev@users.noreply.github.com>
2023-05-25 10:59:01 +0100

    #17: Reroute the user to '/:room' when they click (#1)
    * Reroute the user to '/:room' when they click
    
    * Reroute the user to a random url when they click a btn
    
    * Reroute the user to a random url when they click a btn
    
    * Fix the naming & the error handling
    
    * Reroute the user to '/:room' when they click
    
    * add axios package
    
    * Update link to button and use useNavigate
    
    * Remove Link import
    
    ---------
    
    Co-authored-by: Mario Nikhil Pereira <45009203+nmpereira@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-
-
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { SERVER_URL } from "../common/common";
 
 export default function App() {
-  
+  const [slugName, setSlugName] = useState<string>("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get(`${SERVER_URL}/api/v1/getSlug`)
+      .then((res) => {
+        setSlugName(res.data.slug);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div className="App">
-      
-    <h1>Hello</h1>
+      <h1>Community Focus</h1>
+      <button onClick={() => navigate(`/${slugName}`)}>Join a room</button>
     </div>
   );
 }
