d825d0fc95f66cd1847945fd3c8c063378b54a27
riddlew <riddle.w@gmail.com>
2023-05-24 23:36:40 -0400

    Add airbnb eslint settings and prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,81 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    jest: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'airbnb',
+    'airbnb-typescript/base',
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-refresh',
+    'unused-imports',
+    'prefer-arrow',
+    'prettier',
+  ],
   rules: {
+    'prettier/prettier': ['error'],
     'react-refresh/only-export-components': 'warn',
+    "@typescript-eslint/no-unused-vars": ["error"],
+    // "react/prop-types": "off",
+    "react/jsx-filename-extension": [
+      1,
+      {
+        "extensions": [".tsx"]
+      }
+    ],
+    "react/jsx-props-no-spreading": "off",
+    "react/function-component-definition": [
+      "error", {
+        "namedComponents": "arrow-function",
+        "unnamedComponents": "arrow-function",
+      },
+    ],
+    "import/prefer-default-export": "warn",
+    "import/extensions": ["error", "never"],
+    "no-plusplus": "off",
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-uses-react': 0,
+    "radix": ["error", "as-needed"],
+    "no-restricted-syntax": "off",
+    "@typescript-eslint/explicit-function-return-type": "warn",
+    "unused-imports/no-unused-imports": "error",
+    "prefer-arrow/prefer-arrow-functions": [
+      "warn", {
+        "disallowPrototype": true,
+        "singleReturnOnly": false,
+        "classPropertiesAllowed": false,
+      }
+    ],
+    "prefer-arrow-callback": [
+      "warn", {
+        "allowNamedFunctions": true
+      },
+    ],
+    "func-style": [
+      "warn",
+      "expression",
+      {
+        "allowArrowFunctions": true
+      },
+    ],
   },
 }
