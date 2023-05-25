ba434f6351099836d4784073830d929e02751abd
Mario Nikhil Pereira <45009203+nmpereira@users.noreply.github.com>
2023-05-25 05:52:08 -0400

    Remove Link import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { SERVER_URL } from "../common/common";
