3e3f69f2dfdbe6e6a622d7e731b7a9dcfb8e81c0
riddlew <riddle.w@gmail.com>
2023-05-25 22:16:46 -0400

    Refactor code with eslint and prettier formats

--- src/helpers/formatTimestamp.ts
+++ src/helpers/formatTimestamp.ts
@@ -1,15 +1,14 @@
-function formatTimestamp(timeRemaining: number) {
+const formatTimestamp = (timeRemaining: number): string => {
 	const hours = Math.floor(timeRemaining / (60 * 60));
 	const minutes = Math.floor((timeRemaining % (60 * 60)) / 60);
-	const seconds = Math.floor((timeRemaining % 60));
-	const padZeros = (num: number) => num.toString().padStart(2, '0');
+	const seconds = Math.floor(timeRemaining % 60);
+	const padZeros = (num: number): string => num.toString().padStart(2, "0");
 
 	// Sheds "HH:" when duration is under 1 hour (3600 seconds)
 	if (timeRemaining >= 3600) {
 		return `${padZeros(hours)}:${padZeros(minutes)}:${padZeros(seconds)}`;
-	} else {
-		return `${padZeros(minutes)}:${padZeros(seconds)}`
 	}
-}
+	return `${padZeros(minutes)}:${padZeros(seconds)}`;
+};
 
 export default formatTimestamp;
