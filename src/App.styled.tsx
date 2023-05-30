8428b7729f94d135c3ee7ae12c91eed68fc92a90
Mario Nikhil Pereira <45009203+nmpereira@users.noreply.github.com>
2023-05-29 20:56:35 -0400

    linter changes

--- src/App.styled.tsx
+++ src/App.styled.tsx
@@ -1,8 +1,8 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Title = styled.h1`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+`;
