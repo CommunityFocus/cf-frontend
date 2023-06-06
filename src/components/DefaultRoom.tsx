663acb8cbca5300cd77438c61939730c06a46e2b
Robert <63929802+cashmerefrosh911@users.noreply.github.com>
2023-06-06 18:27:42 -0400

    Added a logo text

--- src/components/DefaultRoom.tsx
+++ src/components/DefaultRoom.tsx
@@ -1,11 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import Footer from "./Footer";
+import LogoTitle from "./Logo/LogoTitle";
 
 const DefaultRoom = (props: { globalUsersConnected: number }): JSX.Element => {
 	const { globalUsersConnected } = props;
 	const Navigate = useNavigate();
 	return (
 		<>
+			<LogoTitle />
 			<h1>This room is not available.</h1>
 			<button
 				type="button"
---
+++ src/components/Logo/LogoTitle.tsx
@@ -0,0 +1,11 @@
+import { LogoText } from "./LogoTitle.styled"
+
+const LogoTitle = () => {
+	return (
+		<div>
+			<LogoText>CommunityFocus</LogoText>
+		</div>
+	)
+}
+
+export default LogoTitle
