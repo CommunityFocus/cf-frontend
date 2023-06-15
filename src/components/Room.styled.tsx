0e78f1fc227251c13251a6f5b62986cb2faf1cfd
AhmedHHamdy <ahmedhhamdyad@gmail.com>
2023-06-15 05:08:43 +0300

    Refactoring code in Room.styled.tsx

--- src/components/Room.styled.tsx
+++ src/components/Room.styled.tsx
@@ -1,10 +1,9 @@
-import styled from "styled-components";
-import { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from "styled-components";
 
 export const StyledMain = styled.main`
-    background: #C7DDC9;
-    height: 93vh;
-    padding: 1%;
+	background: #c7ddc9;
+	height: 93vh;
+	padding: 1%;
 `;
 
 export const GlobalStyle = createGlobalStyle`
