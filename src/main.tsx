81f015137288a949b970481c0742481a3dabcaa4
Christopher Jin <chrisjin1@gmail.com>
2023-06-24 09:16:27 -0400

    remove comments on main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Room from "./components/Room.tsx";
 import DefaultRoom from "./components/DefaultRoom.tsx";
 import LandingPage from "./components/LandingPage.tsx";
-// import ModalProvider from "./context/ModalContext/Modal.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
