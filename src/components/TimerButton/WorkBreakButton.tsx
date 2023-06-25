98c2c2ee9c95091f4334df8ea311cfc89095e8e0
Adan Ayaz <999a.d.a.n.a.y.a.z999@gmail.com>
2023-06-25 11:45:27 +0500

    Frontend code for the issue #80 and issue #81. (#52)
    * Frontend code for the issue #80 and issue #81.
    
    * Changes for resolving issue #80 and issue #81
    
    * Changes to frontend code for issue#80 and issue#81
    
    ---------
    
    Co-authored-by: Mario Nikhil Pereira <45009203+nmpereira@users.noreply.github.com>

---
+++ src/components/TimerButton/WorkBreakButton.tsx
@@ -0,0 +1,33 @@
+import socket from "../socket";
+
+interface WorkBreakButtonProps {
+	roomName: string;
+	isBreak: boolean;
+}
+
+const WorkBreakButton = ({
+	roomName,
+	isBreak,
+}: WorkBreakButtonProps): JSX.Element => {
+	const onClickHandler = (): void => {
+		if (!isBreak) {
+			socket.emit("breakTimer", {
+				userName: "name-functionality-is-yet-to-be-implemented",
+				roomName,
+			});
+		} else if (isBreak) {
+			socket.emit("workTimer", {
+				userName: "name-functionality-is-yet-to-be-implemented",
+				roomName,
+			});
+		}
+	};
+
+	return (
+		<button type="button" onClick={onClickHandler}>
+			{isBreak ? "Start working" : "Take a break"}
+		</button>
+	);
+};
+
+export default WorkBreakButton;
