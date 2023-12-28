0d839bad7fe70e792f21c1a553b7dc5446c967fb
Mario Nikhil Pereira <45009203+nmpereira@users.noreply.github.com>
2023-12-28 01:09:51 -0500

    remove css

--- src/components/RoomList/RoomList.styled.tsx
+++ src/components/RoomList/RoomList.styled.tsx
@@ -1,55 +1,58 @@
 import styled from "styled-components";
 
 export const RoomListTable = styled.table`
-	// width: 100%;
-	border-collapse: collapse;
+	// // width: 100%;
+	// border-collapse: collapse;
 
-	width: 60%;
-	// height: 100px;
+	// width: 60%;
+	// // height: 100px;
 
-	// overflow: none;
+	// // overflow: none;
 `;
 
 export const RoomListTableBody = styled.tbody`
-	// height: 200px;
-	// overflow-y: visible;
+	// // height: 200px;
+	// overflow: scroll;
+
+	// height: 100px;
+	// overflow: scroll;
 `;
 
 export const RoomListTableHeader = styled.thead`
-	border: 1px solid black;
-	padding: 10px;
+	// border: 1px solid black;
+	// padding: 10px;
 `;
 
 export const RoomListTableHeaderCell = styled.th`
-	border: 1px solid black;
-	padding: 10px;
-	width: 33%;
+	// border: 1px solid black;
+	// padding: 10px;
+	// width: 33%;
 `;
 
 export const RoomListTableRow = styled.tr`
-	border: 1px solid black;
-	padding: 10px;
-	height: 5px;
+	// border: 1px solid black;
+	// padding: 10px;
+	// height: 5px;
 `;
 
 export const RoomListTableRowCell = styled.td`
-	border: 1px solid black;
-	padding: 10px;
+	// border: 1px solid black;
+	// padding: 10px;
 
-	width: 33%;
+	// width: 33%;
 
-	text-align: center;
-	vertical-align: middle;
+	// text-align: center;
+	// vertical-align: middle;
 `;
 
 export const RoomListTableButton = styled.button`
-	width: 50%;
-	height: 100%;
-	background-color: #4caf50;
-	color: white;
+	// width: 50%;
+	// height: 100%;
+	// background-color: #4caf50;
+	// color: white;
 
-	border: none;
-	border-radius: 5px;
-	padding: 5px;
-	text-align: center;
+	// border: none;
+	// border-radius: 5px;
+	// padding: 5px;
+	// text-align: center;
 `;
