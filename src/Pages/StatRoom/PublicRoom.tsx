c7c3c5abf87c0390301d8892a244a79a8dce757c
Mario Nikhil Pereira <45009203+nmpereira@users.noreply.github.com>
2023-12-31 22:27:20 -0500

    commit changes from merge

--- src/Pages/StatRoom/PublicRoom.tsx
+++ src/Pages/StatRoom/PublicRoom.tsx
@@ -3,7 +3,7 @@ import { ThemeContext } from "styled-components";
 import socket from "../../components/Socket/socket";
 import { Centered } from "./StatRoom.styled";
 import Header from "../../components/Header/Header";
-import { GlobalStyle } from "../Room/Room.styled";
+
 import { theme } from "../../../common/theme";
 import Footer from "../../components/Footer/Footer";
 import ConnectionState from "../../components/ConnectionState/ConnectionState";
@@ -11,6 +11,7 @@ import ConnectionState from "../../components/ConnectionState/ConnectionState";
 import PublicRoomRouter from "./PublicRoomRouter";
 import { AdminContext } from "../../../common/common";
 import { ITimerRooms } from "../../components/RoomList/RoomList";
+import { GlobalStyle } from "../../components/Room/Room.styled";
 
 export interface IPublicRoom {
 	isBreak: boolean;
